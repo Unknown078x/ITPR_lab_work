8267870d50f762205d355b8a215fac390738ef6d
Unknown078x <Prajapati.shiva.125@gmail.com>
2025-10-16 10:31:10 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,32 @@
+package anudip;
+
+
+class SI{
+        private float principal,rate;
+        private int time;
+        SI(float p, float r, int t){
+            principal=p;
+            rate=r;
+            time=t;
+        }
+        public void DisplayData(){
+            System.out.println("------------Simple Interest------------");
+            System.out.println("Principal: "+principal+"Rs.");
+            System.out.println("Rate: "+rate+"%");
+            System.out.println("Time: "+time);
+        }
+        public void CalculateSI(){
+            float si = (principal*rate*time)/100;
+            System.out.println("--------------------------------------");
+            System.out.print("Simple Interest is: " + si);
+        }
+    }
+public class SimpleInterest {
+
+    public static void main(String[] args) {
+        SI a=new SI(1500f,10f,5);
+
+        a.DisplayData();
+        a.CalculateSI();
+    }
+}
